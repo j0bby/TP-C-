dbe2c4327de2c9165afad582f5c9a7d37c640210
Marc Fallouh <marc.fallouh@insa-lyon.fr>
2015-11-30 15:44:10 +0100

    Tout au même endroit c'est mieux

---
+++ TP3/TP3/Cible.cpp
@@ -0,0 +1,72 @@
+/*************************************************************************
+Cible  -  description
+-------------------
+début                : 2015
+copyright            : (C) 2015 par mfallouh mvirsolvy
+*************************************************************************/
+
+//---------- Réalisation de la classe <Cible> (fichier Cible.cpp) --
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+
+//------------------------------------------------------ Include personnel
+#include <Cible.h>
+//------------------------------------------------------------- Constantes
+
+//---------------------------------------------------- Variables de classe
+
+//----------------------------------------------------------- Types privés
+
+
+//----------------------------------------------------------------- PUBLIC
+//-------------------------------------------------------- Fonctions amies
+
+//----------------------------------------------------- Méthodes publiques
+
+
+/*//------------------------------------------------- Surcharge d'opérateurs
+Cible & Cible::operator = ( const Cible & unCible )
+// Algorithme :
+//
+{
+} //----- Fin de operator =
+
+*/
+//-------------------------------------------- Constructeurs - destructeur
+Cible::Cible(const Cible & unCible)
+// Algorithme :
+//
+{
+#ifdef MAP
+	cout << "Appel au constructeur de copie de <Cible>" << endl;
+#endif
+} //----- Fin de Cible (constructeur de copie)
+
+
+Cible::Cible()
+// Algorithme :
+//
+{
+#ifdef MAP
+	cout << "Appel au constructeur de <Cible>" << endl;
+#endif
+} //----- Fin de Cible
+
+
+Cible::~Cible()
+// Algorithme :
+//
+{
+#ifdef MAP
+	cout << "Appel au destructeur de <Cible>" << endl;
+#endif
+} //----- Fin de ~Cible
+
+
+  //------------------------------------------------------------------ PRIVE
+
+  //----------------------------------------------------- Méthodes protégées
+
+  //------------------------------------------------------- Méthodes privées
