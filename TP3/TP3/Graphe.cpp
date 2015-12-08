758284c845e7a91dbe86b579da212ad75afe3248
j0bby <mathieuvirsolvy@gmail.com>
2015-12-09 00:44:36 +0100

    Progression dans la classe Graphe.cpp

--- TP3/TP3/Graphe.cpp
+++ TP3/TP3/Graphe.cpp
@@ -12,13 +12,13 @@
 //-------------------------------------------------------- Include système
 using namespace std;
 #include <iostream>
-
+#include <string>
 //------------------------------------------------------ Include personnel
 #include "Graphe.h"
 
 //------------------------------------------------------------- Constantes
 const int NB_FORMAT_IMAGE=7;
-const string[NB_FORMAT_IMAGE] FORMAT_IMAGE= {".jpg",".png",".jpeg",".tga",".gif",".tiff", ".bmp"};
+const string FORMAT_IMAGE = ".jpg";// , ".png", ".jpeg", ".tga", ".gif", ".tiff", ".bmp" };
 
 
 //---------------------------------------------------- Variables de classe
@@ -64,17 +64,35 @@ Graphe::Graphe ( const Collection &aCol, const bool e = false, const int h = -1 
 	map<string , Cible > :: const_iterator debut,fin;
 	debut=aCol.pages.begin();
 	fin=aCol.pages.end();
-	for(debut ; debut!=fin; debut++)
+	for(debut ; debut!=fin; debut++) // itération de parcours de la map pages
 	{
+		bool estImage = false;
+
 		if(e) // filtre en fonction des extensions
 		{
-			for(int i=0; i<NB_FORMAT_IMAGE; i++)
+			size_t posExtension = debut->first.find_last_of('.'); // position à partir de laquelle commence l'extension
+			string Extension = debut->first.substr(posExtension, debut->first.npos); // l'extension
+			unsigned int i = 0;
+			while(!estImage && i<NB_FORMAT_IMAGE) // parcours les extensions images.
+			{
+				estImage = Extension.compare(FORMAT_IMAGE) == 0;// si l'extension est celle d'une image
+			}
+		}
+		if(h!=-1 && !estImage) // filtre en fonction de l'heure
+		{
+			map<string, list<Log> > ::const_iterator typeReqDeb,typeReqFin;
+			typeReqDeb = debut->second.lesLogs[h].begin();
+			typeReqFin = debut->second.lesLogs[h].end();
+			while (debut!=fin && debut->first!="GET")
+			{
+				debut++;
+			}
+			if (debut->first != "GET") 
 			{
 
 			}
 		}
-		if(h!=-1) // filtre en fonction de l'heure
-		{
+		else if (h == -1 && !estImage) {
 
 		}
 	}
